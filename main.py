6fbc964181d1cbdcf203aad73e5d04a38c75e386
Muddy Landerson <muddy+github@hutmail.cc>
2025-01-25 14:45:17 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import os, sys
+from flask import Flask, render_template, request, Response, jsonify
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():    
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
